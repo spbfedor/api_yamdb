6c38987f26d099f00ea39862d780f0e04d870883
Fedor <spbfeddor@yandex.ru>
2022-06-26 22:22:34 +0300

    Добавил модели Title, Categoty, Genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+
 class Titles(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    year = models.DateField()
+    description = models.CharField(max_length=200)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles'
+    )
